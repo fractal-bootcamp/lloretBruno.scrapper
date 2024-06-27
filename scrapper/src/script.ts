f3a2452ecdbcfd4da6fb5ab10ac2314d64b366c8
brunoLloret <163921771+brunoLloret@users.noreply.github.com>
2024-06-27 15:14:00 -0400

    getAnchorTags with Cheerio

--- scrapper/src/script.ts
+++ scrapper/src/script.ts
@@ -1,4 +1,5 @@
 
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch'
 
 
@@ -42,10 +43,10 @@ import fetch from 'node-fetch'
 //         console.error('Error fetching data:', error);
 //         return '';
 //     }
-// }
+// // }
 
 
-export const fetchFunction = async (url: string): Promise<string | null> => {
+export const fetchFunction = async (url: string): Promise<string> => {
     try {
 
         const response = await fetch(url);
@@ -53,6 +54,7 @@ export const fetchFunction = async (url: string): Promise<string | null> => {
 
         let data;
 
+        // we need to define what type of content is it being fetched
         if (contentType && contentType.includes('text/html')) {
 
             data = await response.text();
@@ -69,7 +71,10 @@ export const fetchFunction = async (url: string): Promise<string | null> => {
 
         }
 
-        return JSON.stringify(data)
+        const fetched = JSON.stringify(data)
+
+        return fetched
+
 
 
     } catch (error) {
@@ -78,8 +83,73 @@ export const fetchFunction = async (url: string): Promise<string | null> => {
     }
 }
 
-const fetchPokeData = (await fetchFunction(`https://pokeapi.co/api/v2/pokemon-species/aegislash`))
-const fetchWikipedia = (await fetchFunction(`https://en.wikipedia.org/wiki/Arnold_Bennett#Works`))
+// tested !!
+//try it!
+// const fetchPokeData = (await fetchFunction(`https://pokeapi.co/api/v2/pokemon-species/aegislash`))
+const fetchWikipedia = (await fetchFunction(`https://en.m.wikipedia.org/wiki/Wikipedia:Very_short_featured_articles`))
 
-console.log('the new data is:', fetchPokeData)
-console.log('the new data is:', fetchWikipedia)
+// console.log('the new data is:', fetchPokeData)
+// console.log('the new data is:', fetchWikipedia)
+
+
+
+
+// const fetchDataSize = async (url: string): Promise<{ bytes: number; kilobytes: string; megabytes: string; }> => {
+
+//     try {
+
+//         const response = await fetch(url);
+//         const contentLength = response.headers.get('content-length');
+
+//         if (contentLength) {
+//             const sizeInBytes = parseInt(contentLength);
+//             const sizeInKilobytes = sizeInBytes / 1024;
+//             const sizeInMegabytes = sizeInKilobytes / 1024;
+
+//             return {
+//                 bytes: sizeInBytes,
+//                 kilobytes: sizeInKilobytes.toFixed(2),
+//                 megabytes: sizeInMegabytes.toFixed(2)
+//             };
+
+//         } else {
+//             throw new Error('Content-Length header not found');
+//         }
+//     } catch (error) {
+//         throw new Error(`Failed to get file size from URL: ${error.message}`);
+//     }
+// }
+
+
+// tested !!
+//try it!
+// const fetchPokeDataSize = (await fetchDataSize(`https://pokeapi.co/api/v2/pokemon-species/aegislash`))
+// const fetchWikipediaSize = (await fetchDataSize(`https://en.wikipedia.org/wiki/Arnold_Bennett#Works`))
+
+// console.log('the new data is:', fetchPokeDataSize)
+// console.log('the new data is:', fetchWikipediaSize)
+
+
+
+
+// there's another version. Nobody thinks of the children while traversing the DOM: https://cheerio.js.org/docs/basics/traversing
+
+const getAnchorTags = (html: string): string[] | undefined => {
+
+    const $ = cheerio.load(html)
+
+    const findLinks = $('section').find('a')
+
+    const mapingOverAnchors = findLinks.map((index, element) => {
+        return $(element).attr('href');
+    })
+
+    const getTheAnchorsHREFS = mapingOverAnchors.get();
+
+    return getTheAnchorsHREFS
+}
+
+
+const newData = getAnchorTags(fetchWikipedia)
+
+console.log(newData)
